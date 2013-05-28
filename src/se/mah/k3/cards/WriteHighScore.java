37a8af06d083cc927b6c9a2c2a0bc37a757d486b
Emil Sjunnesson <byrhult@gmail.com>
2013-05-28 11:27:38 +0200

    textview i higscoreclassen

--- src/se/mah/k3/cards/WriteHighScore.java
+++ src/se/mah/k3/cards/WriteHighScore.java
@@ -1,7 +1,18 @@
 package se.mah.k3.cards;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class WriteHighScore extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.write_highscore);
+		String highScore = Integer.toString(getIntent().getExtras().getInt(
+				"score"));
+		TextView highScoretv = (TextView) findViewById(R.id.HSText);
+		highScoretv.setText(highScore);
+	}
 }

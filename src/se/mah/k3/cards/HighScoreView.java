c7618d76177c18fbef5aa7607588bd472f0e0273
Simon Lam <simon.t.lam@gmail.com>
2013-05-29 16:52:41 +0200

    Hej
    gjort ny klass och xml för highscore

---
+++ src/se/mah/k3/cards/HighScoreView.java
@@ -0,0 +1,16 @@
+package se.mah.k3.cards;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HighScoreView extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.highscore_view);
+	}
+	
+	
+}

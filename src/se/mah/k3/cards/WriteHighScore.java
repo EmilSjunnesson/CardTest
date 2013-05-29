9a0b839873d51d069b06764d2ca1e313a85c5d6c
Emil Sjunnesson <byrhult@gmail.com>
2013-05-29 16:19:54 +0200

    Highscore början

--- src/se/mah/k3/cards/WriteHighScore.java
+++ src/se/mah/k3/cards/WriteHighScore.java
@@ -2,17 +2,16 @@ package se.mah.k3.cards;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class WriteHighScore extends Activity {
-
+	
+	private long score;
+	private String name;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.write_highscore);
-		String highScore = Integer.toString(getIntent().getExtras().getInt(
-				"score"));
-		TextView highScoretv = (TextView) findViewById(R.id.HSText);
-		highScoretv.setText(highScore);
+		score = getIntent().getExtras().getLong("score");
 	}
 }

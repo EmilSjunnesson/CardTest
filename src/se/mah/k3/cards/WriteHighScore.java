d5fcffabfbaec9062c8e67e7d1ed0c6af3f32810
Philip Joensson <bengt_philip@hotmail.com>
2013-05-30 15:53:16 +0200

    WriteHighScore class KLAR

--- src/se/mah/k3/cards/WriteHighScore.java
+++ src/se/mah/k3/cards/WriteHighScore.java
@@ -1,6 +1,7 @@
 package se.mah.k3.cards;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -14,27 +15,33 @@ public class WriteHighScore extends Activity {
 	private String name;
 	EditText player;
 	TextView scoreview;
-	
+	Highscore highscore;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.write_highscore);
+		score = getIntent().getExtras().getLong("score");
 		
 		player = (EditText)findViewById(R.id.player); //edittext komponent
 		scoreview = (TextView)findViewById(R.id.scoreview); //textview komponent
-		
+		highscore = new Highscore(this);
+		scoreview.setText(String.valueOf(score));
 		player.setOnKeyListener(new OnKeyListener() {
 			
 			@Override
 			public boolean onKey(View v, int keyCode, KeyEvent event) {
 				// TODO Auto-generated method stub
 				if((event.getAction() == KeyEvent.ACTION_DOWN)&&(keyCode == KeyEvent.KEYCODE_ENTER)){
-					
+					name=player.getText().toString();
+					highscore.addScore(name, score);
+					Intent intent = new Intent(WriteHighScore.this,HighScoreView.class);
+					startActivity(intent);
+					finish();
 				}
 				return false;
 			}
 		});
 		
-		score = getIntent().getExtras().getLong("score");
+		
 	}
 }

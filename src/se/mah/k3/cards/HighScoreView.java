2db4d1ecd25c296108d0782e97d36f5bc4669f84
Philip Joensson <bengt_philip@hotmail.com>
2013-05-30 16:19:27 +0200

    Skapat textview i viewklassen

--- src/se/mah/k3/cards/HighScoreView.java
+++ src/se/mah/k3/cards/HighScoreView.java
@@ -2,8 +2,14 @@ package se.mah.k3.cards;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HighScoreView extends Activity {
+
+	TextView score1, score2, score3, score4, score5, score6, score7, score8,
+			score9, score10, name1, name2, name3, name4, name5, name6, name7,
+			name8, name9, name10;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +17,5 @@ public class HighScoreView extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.highscore_view);
 	}
-	
-	
+
 }

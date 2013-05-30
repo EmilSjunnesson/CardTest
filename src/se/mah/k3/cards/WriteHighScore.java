65d00c4de3201e56d0d265d9c5a86a472294ba35
Philip Joensson <bengt_philip@hotmail.com>
2013-05-30 15:36:28 +0200

    XML ändrar bytt namn på textview & edittext.

--- src/se/mah/k3/cards/WriteHighScore.java
+++ src/se/mah/k3/cards/WriteHighScore.java
@@ -2,16 +2,39 @@ package se.mah.k3.cards;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class WriteHighScore extends Activity {
 	
 	private long score;
 	private String name;
+	EditText player;
+	TextView scoreview;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.write_highscore);
+		
+		player = (EditText)findViewById(R.id.player); //edittext komponent
+		scoreview = (TextView)findViewById(R.id.scoreview); //textview komponent
+		
+		player.setOnKeyListener(new OnKeyListener() {
+			
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				// TODO Auto-generated method stub
+				if((event.getAction() == KeyEvent.ACTION_DOWN)&&(keyCode == KeyEvent.KEYCODE_ENTER)){
+					
+				}
+				return false;
+			}
+		});
+		
 		score = getIntent().getExtras().getLong("score");
 	}
 }

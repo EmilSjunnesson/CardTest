874068b1729ca590db31c672b1f06e134668ec18
Emil Sjunnesson <byrhult@gmail.com>
2013-05-30 12:06:42 +0200

    commit

--- src/se/mah/k3/cards/MainActivity.java
+++ src/se/mah/k3/cards/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		//commit
 		// fullscreen
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,

1fbf36db86b58def36fcac0d66eb4429835a1891
cain666 <not_abel@hotmail.com>
2013-06-04 18:26:56 +0200

    Splashen

--- src/se/mah/k3/cards/SplashScreen.java
+++ src/se/mah/k3/cards/SplashScreen.java
@@ -1,45 +1,89 @@
 package se.mah.k3.cards;
 
+
+
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 
 public class SplashScreen extends Activity {
-
+	
+	
+	
+	
+	ProgressBar myprogress;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		// fullscreen
+		
+		// Fullscreen
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-		setContentView(R.layout.splash);
-
-		IntentLauncher launcher = new IntentLauncher();
-		launcher.start();
-
+		
+		setContentView(R.layout.splash);						
+		myprogress=(ProgressBar) findViewById(R.id.myprogress);
+		new ShowDialogAsyncTask().execute();
+		
 	}
+	private class ShowDialogAsyncTask extends AsyncTask<Void, Integer, Void>{
+		 
+	    int progress_status;
+	     
+	    @Override
+	 protected void onPreExecute() {
+	  super.onPreExecute();
+	   
+	   
 
-	private class IntentLauncher extends Thread {
-
-		@Override
-		public void run() {
-			try {
-				Thread.sleep(2000);
-			} catch (Exception e) {
-				Log.e("TagBag", "could not load application");
-			}
-
-			// Start main activity
-			Intent intent = new Intent(SplashScreen.this, StartScreen.class);
+	   progress_status = 0;
+	  
+	   
+	   
+	 }
+	 
+	    @Override
+		  protected Void doInBackground(Void... params) {
+		    
+		   while(progress_status<100){
+		     
+		    progress_status += 2;
+		     
+		    publishProgress(progress_status);
+		    SystemClock.sleep(80);
+		     
+		   }
+		   return null;
+		  }
+		
+	    @Override
+		  protected void onProgressUpdate(Integer... values) {
+		   super.onProgressUpdate(values);
+		    
+		   myprogress.setProgress(values[0]);
+		  
+		 
+		  }
+		   
+		  @Override
+		  protected void onPostExecute(Void result) {
+		   super.onPostExecute(result);
+		   Intent intent = new Intent(SplashScreen.this, StartScreen.class);
 			SplashScreen.this.startActivity(intent);
 			SplashScreen.this.finish();
-		}
-	}
-
+		     		   
+		  }
+		    }		
 }
